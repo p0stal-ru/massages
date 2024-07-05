eaf7bc51fd31c5b5dcfd2b45bf5e72efe91a0469
Dmitriy Kozhemyakin <70subali@gmail.com>
2024-07-05 20:22:08 +0300

    first commit

---
+++ src/test/java/com/example/massages/MassagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.massages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
